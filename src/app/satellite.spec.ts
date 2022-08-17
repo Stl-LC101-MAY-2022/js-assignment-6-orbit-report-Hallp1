c890077009d2a63af5d3c7c6a2977ae309149efe
Phall <hallpenelope@rocketmail.com>
2022-08-17 10:17:50 -0500

    let

--- src/app/satellite.spec.ts
+++ src/app/satellite.spec.ts
@@ -1,19 +1,7 @@
 import { Satellite } from './satellite';
 
 describe('Satellite', () => {
-  let satellite: Satellite;
-  beforeEach(() => {
-    satellite = new Satellite("foo", "bar", "baz", "lur", false);
-  });
-
-  afterEach(() => {
-	  satellite = null;
-  });
-
   it('should create an instance', () => {
-    expect(satellite).toBeTruthy();
+    expect(new Satellite()).toBeTruthy();
   });
-
-
- 
 });
